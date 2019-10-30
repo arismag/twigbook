586c073154525908a909ed5d29e5da3b3b846f15
Bill Seremetis <bill@seremetis.net>
2019-10-30 09:03:03 +0200

    Add a few addons.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
 import { configure } from '@storybook/html';
 
+// Addons
+import '@storybook/addon-console';
+
 // automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.js$/), module);
+  configure(require.context('../stories', true, /\.stories\.js$/), module);
